3fb598d29ba64b933d0145bac246fc5a26103549
Anton Kuzniatsou <anton.kuzniatsou@designory.com>
2017-06-27 12:51:52 -0500

    updated api for status search

--- routes/tickets.js
+++ routes/tickets.js
@@ -4,7 +4,7 @@ var express = require('express'),
 
 /* GET tickets from jiraStat mongoDB */
 router.get('/', function(req, res, next) {
-
+	console.log(req.query.status);
 	switch (req.query.status) {
 
 		case 'open':
@@ -12,9 +12,16 @@ router.get('/', function(req, res, next) {
 					res.json(results);
 			});
 			break;
-			
+
 		case 'inProgress':
 			mongo.find('tickets', {"fields.status.id": "10037" }, function(results) {
+					res.json(results);
+			});
+			break;
+
+		case 'open inProgress':
+		case 'inProgress open':
+			mongo.find('tickets', {"fields.status.id": { $in: ["1", "4", "10037"]} }, function(results) {
 					res.json(results);
 			});
 			break;
