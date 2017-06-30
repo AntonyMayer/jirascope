92bdce1b3503632a899ee8873fdba21b39e122ae
Anton Kuzniatsou <anton.kuzniatsou@designory.com>
2017-06-30 09:51:50 -0500

    title update

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Jirascope' });
+  res.render('index', { title: 'The Dark Side of Jirascope' });
 });
 
 module.exports = router;
