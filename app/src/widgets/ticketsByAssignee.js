a798f3dcb3de90df4eb103f39ac2122e4e9c6004
Anton Kuzniatsou <anton.kuzniatsou@designory.com>
2017-07-11 12:32:07 -0500

    widget in progress

--- app/src/widgets/ticketsByAssignee.js
+++ app/src/widgets/ticketsByAssignee.js
@@ -7,76 +7,54 @@ export default function() {
             return res.json();
         })
         .then(tickets => {
-            let projects = {}, //temporary object for projects
-                table = [ //will be passed to Table component
-                    [ //headers (first row)
-                        'Project',
-                        'Key',
-                        'Open',
-                        'In Progress', 
-                        'Dev Complete',
-                        'Dev Test',
-                        'Tridion Publishing',
-                        'Ready for test',
-                        'Blocked',
-                        'Closed',
-                        'Assignees'
-                    ]
-                ],
-                total = { //object contains total counters
-                    opened: 0,
-                    inProgress: 0,
-                    devComplete: 0,
-                    devTest: 0,
-                    tridion: 0,
-                    readyForTest: 0,
-                    blocked: 0,
-                    closed: 0
-                };
+            let assignees = {}, //temporary object for projects
+                table = [],
+                headers = [];
 
             //create project and its counters
             tickets.forEach(ticket => {
-                if (!projects[ticket.fields.project.key]) {
-                    createRecord(ticket, projects);
-                    updateRecord(ticket, projects, total);
+                if (!assignees[ticket.fields.assignee.key]) {
+                    createRecord(assignees, ticket.fields.assignee.key);
+                    updateRecord(assignees, ticket.fields.assignee.key, ticket.fields.project.key);
+                    headers.push(ticket.fields.project.key);
                 } else {
-                    updateRecord(ticket, projects, total);
+                    updateRecord(assignees, ticket.fields.assignee.key, ticket.fields.project.key);
                 }
             });
 
             //fill table 
-            for (let project in projects) {
-                let row = [
-                    clearProjectName(projects[project]['name']), //full project name
-                    project, //project key
-                    projects[project]['opened'], //number tickets
-                    projects[project]['inProgress'],
-                    projects[project]['devComplete'],
-                    projects[project]['devTest'],
-                    projects[project]['tridion'],
-                    projects[project]['readyForTest'],
-                    projects[project]['blocked'],
-                    projects[project]['closed'],
-                    projects[project]['assignees'].join(', ') //list of assignees                
-                ];
-                if (checkRow(row.slice(2,9))) table.push(row);
-            }
+            // for (let project in projects) {
+            //     let row = [
+            //         clearProjectName(projects[project]['name']), //full project name
+            //         project, //project key
+            //         projects[project]['opened'], //number tickets
+            //         projects[project]['inProgress'],
+            //         projects[project]['devComplete'],
+            //         projects[project]['devTest'],
+            //         projects[project]['tridion'],
+            //         projects[project]['readyForTest'],
+            //         projects[project]['blocked'],
+            //         projects[project]['closed'],
+            //         projects[project]['assignees'].join(', ') //list of assignees                
+            //     ];
+            //     if (checkRow(row.slice(2,9))) table.push(row);
+            // }
 
             //add totals to the table
-            table.push([
-                'TOTAL',
-                '-',
-                total['opened'], //number tickets
-                total['inProgress'],
-                total['devComplete'],
-                total['devTest'],
-                total['tridion'],
-                total['readyForTest'],
-                total['blocked'],
-                '-',
-                // total['closed'],
-                '-'
-            ]);
+                // table.push([
+                //     'TOTAL',
+                //     '-',
+                //     total['opened'], //number tickets
+                //     total['inProgress'],
+                //     total['devComplete'],
+                //     total['devTest'],
+                //     total['tridion'],
+                //     total['readyForTest'],
+                //     total['blocked'],
+                //     '-',
+                //     // total['closed'],
+                //     '-'
+                // ]);
 
             /**
              * Return multidimensional array
@@ -89,118 +67,19 @@ export default function() {
              *    [cell, cell, ..., cell]   //row
              * ]
              */
+            console.log(assignees);
+            console.log(headers);
             return table;
         })
 }
 
-function createRecord(ticket, projects) {
-    projects[ticket.fields.project.key] = {
-        name: ticket.fields.project.name,
-        project: ticket.fields.project.key,
-        opened: 0,
-        inProgress: 0,
-        devComplete: 0,
-        devTest: 0,
-        tridion: 0,
-        readyForTest: 0,
-        blocked: 0,
-        closed: 0,
-        assignees: []
-    }
+function createRecord(assignees, assignee) {
+    assignees[assignee] = {};
 }
 
-function updateRecord(ticket, projects, total) {
-
-    /**
-     * CHEATLIST Transition's IDs:
-     * 
-     * 1        "Open"
-     * 4        "Reopened"
-     * 6        "Closed"
-     * 10008    "Ready for Test"
-     * 10035    "Blocked"
-     * 10037    "In Progress"
-     * 10076    "Dev Complete"
-     * 10976    "Developer Test"
-     * 10678    "Parking Lot"
-     * 10977    "Assets Tridion Publishing"
-     * 11276    "HTML Tridion Publishing"
-     * 11076    "Ready for Live"
-     */
-
-    checkTicketFields(ticket);
-
-    let status = ticket.fields.status.id,
-        project = projects[ticket.fields.project.key],
-        currentAssignee = ticket.fields.assignee.key;
-
-    switch (status) {
-        case "1":
-        case "4":
-            project.opened++;
-            total.opened++;
-            updateAssineeList(project, currentAssignee);
-            break;
-        case "10008":
-            project.readyForTest++;
-            total.readyForTest++;
-            updateAssineeList(project, currentAssignee);
-            break;
-        case "10037":
-            project.inProgress++;
-            total.inProgress++;
-            updateAssineeList(project, currentAssignee);
-            break;
-        case "10076":
-            project.devComplete++;
-            total.devComplete++;
-            updateAssineeList(project, currentAssignee);
-            break;
-        case "10976":
-            project.devTest++;
-            total.devTest++;
-            updateAssineeList(project, currentAssignee);
-            break;
-        case "11276":
-        case "10977":
-            project.tridion++;
-            total.tridion++;
-            updateAssineeList(project, currentAssignee);
-            break;
-        case "10035":
-            project.blocked++;
-            total.blocked++;
-            updateAssineeList(project, currentAssignee);
-            break;
-        case "6":
-        case "10038":
-            project.closed++;
-            total.closed++;
-            break;
-        default:
-            // console.log(ticket.key);
-            break;
-    }
-}
-
-/**
- * Check for necessary fields to avoid errors
- * 
- * @param {any} ticket 
- * @returns 
- */
-function checkTicketFields(ticket) {
-    if (ticket.fields.assignee === null) {
-        ticket.fields.assignee = {
-            key: 'Unassigned'
-        }
-    }
-    if (ticket.fields.status === null) {
-        ticket.fields.status = {
-            id: 'Undefined'
-        }
-    }
-    return ticket;
+function updateRecord(assignees, assignee, project) {
+   if (!assignees[assignee][project]) assignees[assignee][project] = 1;
+   else assignees[assignee][project]++;
 }
 
 //eliminate rows with unidentified ticket status and other weird staff (i.e. 0 0 0 0 0 0 0 rows)
